ba71128e6152de0175252262ba992e7676d67b95
JellyApple102 <jcarryer@outlook.com>
2023-03-16 12:12:06 -0400

    add config and setup

--- lua/easyread/init.lua
+++ lua/easyread/init.lua
@@ -1,7 +1,27 @@
 local M = {}
 
+M.config = {
+    hlgroup = 'Bold',
+    fileTypes = { 'text' },
+    saccadeInterval = 0,
+    saccadeReset = false,
+    updateInsertMode = false
+}
+
+M.setup = function (config)
+    M.config = config
+
+    M.namespace = vim.api.nvim_create_namespace('easyread')
+
+    if type(M.config.hlgroup) == 'string' then
+        M.hlgroup = M.config.hlgroup
+    elseif type(M.config.hlgroup) == 'table' then
+        vim.api.nvim_set_hl(0, 'EasyreadHl', M.config.hlgroup)
+        M.hlgroup = 'EasyreadHl'
+    end
+end
+
 M.start = function ()
-    local ns = vim.api.nvim_create_namespace('easyreadns')
     local bufnr = vim.api.nvim_get_current_buf()
     local linecount = vim.api.nvim_buf_line_count(bufnr)
     local lines = vim.api.nvim_buf_get_lines(bufnr, 0, linecount, false)
@@ -10,7 +30,7 @@ M.start = function ()
         -- bold half of each word
         for s, w, e in string.gmatch(line, '()(%w+)()') do
             local half = math.floor(string.len(w) / 2)
-            vim.api.nvim_buf_add_highlight(bufnr, ns, 'Bold', i - 1, s - 1, e - 1 - half);
+            vim.api.nvim_buf_add_highlight(bufnr, M.namespace, M.hlgroup, i - 1, s - 1, e - 1 - half);
         end
     end
 end
