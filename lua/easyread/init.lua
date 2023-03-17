7f56a40c989edd0fea7c08ceeaf2623f20715b41
JellyApple102 <jcarryer@outlook.com>
2023-03-16 23:09:59 -0400

    layout changes for user commands

--- lua/easyread/init.lua
+++ lua/easyread/init.lua
@@ -1,28 +1,8 @@
 local M = {}
 
-M.config = {
-    hlgroup = { link = 'Bold' },
-    fileTypes = { 'text' },
-    saccadeInterval = 0,
-    saccadeReset = false,
-    updateInsertMode = false
-}
-
-M.setup = function (config)
-    M.config = vim.tbl_deep_extend('force', M.config, config or {})
-
-    M.namespace = vim.api.nvim_create_namespace('easyread')
-
-    vim.api.nvim_set_hl(0, 'EasyreadHl', M.config.hlgroup)
-    M.hlgroup = 'EasyreadHl'
-
-    vim.api.nvim_create_user_command('EasyreadClear', function ()
-        local bufnr = vim.api.nvim_get_current_buf()
-        vim.api.nvim_buf_clear_namespace(bufnr, M.namespace, 0, -1)
-    end, {})
-end
-
 M.start = function ()
+    M.clear()
+
     local bufnr = vim.api.nvim_get_current_buf()
     local linecount = vim.api.nvim_buf_line_count(bufnr)
     local lines = vim.api.nvim_buf_get_lines(bufnr, 0, linecount, false)
@@ -53,11 +33,41 @@ M.start = function ()
     end
 end
 
+M.clear = function ()
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_clear_namespace(bufnr, M.namespace, 0, -1)
+end
+
+M.config = {
+    hlgroup = { link = 'Bold' },
+    fileTypes = { 'text' },
+    saccadeInterval = 0,
+    saccadeReset = false,
+    updateInsertMode = false
+}
+
+M.setup = function (config)
+    M.config = vim.tbl_deep_extend('force', M.config, config or {})
+
+    M.namespace = vim.api.nvim_create_namespace('easyread')
+
+    vim.api.nvim_set_hl(0, 'EasyreadHl', M.config.hlgroup)
+    M.hlgroup = 'EasyreadHl'
+
+    vim.api.nvim_create_user_command('EasyreadClear', function ()
+        M.clear()
+    end, {})
+
+    vim.api.nvim_create_user_command('EasyreadStart', function ()
+        M.start()
+    end, {})
+end
+
 return M
 
 -- TODO
 -- [x] add default config and setup function
--- [X] custom highlight group
+-- [x] custom highlight group
 -- [] default on filetypes
 -- [x] implement saccades interval
 --   [x] reset by line or carry over option ??
@@ -65,5 +75,5 @@ return M
 -- -- autocommands?
 -- [] figure out how to determine how much of a word to bold
 -- -- fixation??
--- [-] add vim command(s) e.g. <cmd>EasyreadToggle
+-- [x] add vim command(s) e.g. <cmd>EasyreadToggle?
 -- -- :h nvim_create_user_command()
